54b71f0ce346307c478277f3ed0642cde07d14e9
ruslan <ruslan.abdallah2000@gmail.com>
2022-06-23 11:43:49 +0300

    feat/main: add main component

---
+++ react-education-markup-master/src/components/Main/Main.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import MainContainer, { Form, Info, Stats, Img } from './styled'
+
+function Main() {
+	return (
+		<MainContainer>
+			<Info>
+				<h5>Booking of doctor’s appointment</h5>
+				<h2>Don’t Miss Our Exclusive Patient Special</h2>
+				<p>
+					We want to make sure that everyone has access to natural and effective
+					care. With our special, you’ll receive a consultation and a digital posture
+					assesment.
+				</p>
+				<Form action='post'>
+					<span />
+					<input type='text' name='' id='' placeholder='Enter your email' />
+					<button type='button'>Get Started</button>
+				</Form>
+				<Stats>
+					<li>
+						<span>240</span>
+						<p>Qualified Doctors & Medical Specialists</p>
+					</li>
+					<li>
+						<span>1.456</span>
+						<p>Medical Tests Done For Our Patients</p>
+					</li>
+					<li>
+						<span>1M+</span>
+						<p>Years of Experience The Medical Field</p>
+					</li>
+				</Stats>
+			</Info>
+			<Img />
+		</MainContainer>
+	)
+}
+
+export default Main
---
+++ react-education-markup-master/src/components/Main/styled.ts
@@ -0,0 +1,103 @@
+import styled from 'styled-components'
+
+import mailSvg from '../../assets/icons/mail.svg'
+import bodyImg from '../../assets/images/body-background.webp'
+
+const MainContainer = styled.main`
+	display: flex;
+`
+
+export const Info = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-top: 97px;
+	h2 {
+		font-family: Roboto-Bold;
+		font-size: 65px;
+		line-height: 66px;
+		color: var(--blue);
+		max-width: 543px;
+		margin-bottom: 30px;
+	}
+	h5 {
+		font-family: Roboto-Bold;
+		font-size: 14px;
+		line-height: 24px;
+		color: var(--blue);
+		margin-bottom: 19px;
+	}
+	p {
+		font-size: 20px;
+		line-height: 30px;
+		color: var(--blue);
+		max-width: 520px;
+		margin-bottom: 52px;
+	}
+`
+export const Form = styled.form`
+	display: flex;
+	align-items: center;
+	width: 515px;
+	height: 68px;
+	background: #ffffff;
+	border-radius: 40px;
+	span {
+		margin-left: 22px;
+		background: url(${mailSvg}) no-repeat;
+		width: 24px;
+		height: 24px;
+	}
+	input {
+		border: none;
+		width: 115px;
+		margin-right: 144px;
+		margin-left: 19px;
+		font-size: 16px;
+		line-height: 19px;
+	}
+	button {
+		width: 180px;
+		height: 50px;
+		background: #215ee9;
+		box-shadow: 0px 100px 161px rgba(33, 94, 233, 0.08),
+			0px 64.8148px 94.2894px rgba(33, 94, 233, 0.0607407),
+			0px 38.5185px 51.2815px rgba(33, 94, 233, 0.0485926),
+			0px 20px 26.1625px rgba(33, 94, 233, 0.04),
+			0px 8.14815px 13.1185px rgba(33, 94, 233, 0.0314074),
+			0px 1.85185px 6.33565px rgba(33, 94, 233, 0.0192593);
+		border-radius: 40px;
+		padding: 15px 48px;
+		font-size: 16px;
+		line-height: 19px;
+		color: #ffffff;
+	}
+`
+export const Stats = styled.ul`
+	display: flex;
+	margin-top: 119px;
+	li {
+		margin-right: 78px;
+		max-width: 142px;
+		span {
+			font-size: 56px;
+			line-height: 66px;
+			letter-spacing: -4px;
+			color: #333333;
+			margin-bottom: 13px;
+		}
+		p {
+			font-size: 16px;
+			line-height: 24px;
+			color: #000000;
+		}
+	}
+`
+export const Img = styled.img.attrs({
+	src: bodyImg,
+})`
+	width: 740px;
+	height: 885px;
+	filter: drop-shadow(50px -56px 104px rgba(28, 91, 235, 0.05));
+`
+
+export default MainContainer
